19e0d2880f7f5c33db12154b92113116adda6a46
alireza1420 <alirezafaghihi1383@gmail.com>
2024-04-01 21:12:01 +0330

    adding movemant functions

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -1,13 +1,84 @@
 
 import React, { useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber';
+import { events, useFrame, useThree } from '@react-three/fiber';
 import {a} from '@react-spring/three';
 
 import islandScene from '../assets/3d/island.glb';
-const Island = (props)=> {
+const Island = ({isRotating, setIsRotating, ...props})=> {
     const islandRef=useRef()
   const  { nodes, materials } = useGLTF(islandScene);
+  const {gl, viewport} = useThree();
+
+  const lastX = useRef(0);
+  const rotationSpeed=useRef(0);
+  const dampingFactor = 0.95;
+
+
+
+const handelPointerDown = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+    e.setIsRotating(true);
+
+    const clientX = event.touches ?
+ event.touches[0].clientX 
+: event.clientX;
+
+lastX.current = clientX;
+
+}
+const handelPointerUp = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+    e.setIsRotating(false);
+
+    const clientX = event.touches ?
+    event.touches[0].clientX 
+   : event.clientX;
+
+   const delta = (clientX - lastX.current)  / viewport.width;
+
+   islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+   lastX.current = clientX;
+
+   rotationSpeed.current = delta * 0.01 * Math.PI;
+
+    
+}
+const handelPointerMove = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+
+    if(isRotating){
+        handelPointerUp(e);
+    }
+}
+const handelKeyDown = (e) => {
+    if (e.key === 'ArrowLeft'){
+        if (!isRotating) setIsRotating (true);
+    }
+
+}
+
+
+
+useEffect(()=>{
+document.addEventListener('pointerdown',handelPointerDown)
+document.addEventListener('pointerup',handelPointerUp)
+document.addEventListener('pointermove',handelPointerMove)
+
+return () => {
+
+document.removeEventListener('pointerdown',handelPointerDown)
+document.removeEventListener('pointerup',handelPointerUp)
+document.removeEventListener('pointermove',handelPointerMove)
+
+}
+
+}, [gl, handelPointerDown, handelPointerUp, handelPointerMove])
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
